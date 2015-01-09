b4b71837dd7059618fefaa2b2d07d35db85bef1e
knupur <nupurk15@gmail.com>
2015-01-08 21:05:03 -0500

    Adding comments in  level 1

--- ruby-warrior-challenges/beginner-track/level-1.rb
+++ ruby-warrior-challenges/beginner-track/level-1.rb
@@ -5,6 +5,9 @@
 # Call warrior.walk! to walk forward in the Player 'play_turn' method.
 # 
 
+# Pretty simple! Just make the warrior using the predefined warrior.walk! function. 
+# Success in 1st attemp.
+
 class Player
   def play_turn(warrior)
     warrior.walk!()
