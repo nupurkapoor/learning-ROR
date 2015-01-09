c9257c2ccef9bb407d80e7c012674a3d26536f64
knupur <nupurk15@gmail.com>
2015-01-08 20:57:22 -0500

    Solving level 1

---
+++ ruby-warrior-challenges/beginner-track/level-1.rb
@@ -0,0 +1,12 @@
+##
+# Level 1 challenge
+# 
+# You see before yourself a long hallway with stairs at the end. There is nothing in the way.
+# Call warrior.walk! to walk forward in the Player 'play_turn' method.
+# 
+
+class Player
+  def play_turn(warrior)
+    warrior.walk!()
+  end
+end
